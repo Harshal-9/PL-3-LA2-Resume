9979ef29693f7a62eea53b93da38ba5498e15867
Harshal Kodgire <84096031+Harshal-9@users.noreply.github.com>
2021-10-17 19:54:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,19 @@ document.querySelector(".Button").addEventListener("click",myClick);
 
 function myClick(event)
 {
+    var name=document.querySelector(".name").value;
+    var email=document.querySelector(".email").value;
+    var state = true;
+    
+    if(name=="")
+    state=false;
+
+    if(email=="")
+    state=false;
+
+    if(state)
     alert("form submitted !");
+    else
+    alert("Enter valid data");
 }
+
